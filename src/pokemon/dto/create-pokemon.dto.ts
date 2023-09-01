285fa70c54775de1e8bc5219f3462cd4d2fc38c6
juan sebastian <juan.sebas-722hotmail.com>
2023-09-01 15:54:35 -0500

    dtos and schema built

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, Min, IsString, MinLength} from "class-validator";
+
+
+
+
+
+export class CreatePokemonDto {
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no:number;
+
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+
+}
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,26 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+
+
+
+
+@Schema()
+export class Pokemon extends Document{
+
+    @Prop({
+        unique:true,
+        index:true
+    })
+    name:string;
+
+
+    @Prop({
+        unique:true,
+        index:true,
+    })
+    no: string;
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
